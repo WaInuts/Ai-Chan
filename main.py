433cb2e2cc63f9a4e21069b96d8b142e7907a59b
regadorod <86462898+regadorod@users.noreply.github.com>
2023-07-14 04:22:12 +0000

    Added generate command to send pictures via message.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import os
+import random
+
 from discord.ext import commands
 import discord
-import os
+import requests
+
 from keep_alive import keep_alive
 
 intents = discord.Intents.default()
@@ -13,9 +17,27 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 async def on_ready():
   print('We have logged in as {0.user}'.format(bot))
 
+
 @bot.command()
 async def hello(ctx):
   await ctx.send("Hello!")
+
+@bot.command()
+async def generate(ctx, type):
+  headers = {
+    'User-Agent' : 'Gamer Bot - B Box9688' 
+  }
+  
+  if type == 'girl':
+    waifuTags = {'Female', 'Solo'}
+    id = str(random.randint(1, 1000))
+    r = requests.get(f'https://www.zerochan.net/?p={id}&json',headers=headers)
+    items = r.json()['items']
+
+    for item in items:
+      if waifuTags.issubset(item['tags']):
+        await ctx.send(item['thumbnail'])
+        return
 
 keep_alive()
 bot.run(os.environ['TOKEN'])
