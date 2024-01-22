58fd9472e99beaeed66a18a69d837994f2b9e497
Hector Torres <htorres5@ucsc.edu>
2024-01-22 00:41:18 -0800

    added error checking for cai messages

--- cogs/ai.py
+++ cogs/ai.py
@@ -20,18 +20,9 @@ class AI(commands.Cog):
         if self.bot.user in message.mentions:
             ctx = await self.bot.get_context(message)
 
-            # Format Message for CharacterAI:
-            # - Take away Hu Tao Mention ID 
-            # - Add message author at end, so bot can TRY to differentiate between people in the chat.
-            # TODO: make it more consistent
-            # - Replace @Mentions with raw text
-            bot_name = '@Hu Tao '
-            offset = len(bot_name)
-
             display_name = ctx.message.author.display_name
-            content = message.clean_content # + f" *Stated by: {display_name}*"
+            author_text = message.clean_content
 
-            author_text = content[offset:len(content)]
             print(f'Message: {author_text}')
 
             author = {
@@ -41,11 +32,16 @@ class AI(commands.Cog):
             }
 
             async with message.channel.typing():
-                async with self.cai.client.connect(config.CHARACTER_AI_TOKEN) as chat2:
-                    data = await chat2.send_message(self.cai.char, self.cai.chat_id, author_text, author)
-                text = data['turn']['candidates'][0]['raw_content']
-                print(text)
-
+                try:
+                    async with self.cai.client.connect(config.CHARACTER_AI_TOKEN) as chat2:
+                        data = await chat2.send_message(self.cai.char, self.cai.chat_id, author_text, author)   
+                    text = data['turn']['candidates'][0]['raw_content']
+                    print(f'Hu Tao: {text}')
+                except:
+                    await message.channel.send(f"*sarcastic* I couldn't quite hear you dear, can you repeat what you said? \n`An Error has occured! Please try again. blame @box`")
+                    return
             await message.channel.send(f"{text}")
-            text_to_speech(text)
-            ctx.voice_client.play(discord.FFmpegPCMAudio('voice'))
+
+            # TODO: Add backup TTS
+            #text_to_speech(text)
+            #ctx.voice_client.play(discord.FFmpegPCMAudio('voice'))
